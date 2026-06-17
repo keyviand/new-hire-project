a9417f2dd116c9942c3ea5f9d3fc6eebacecccb6
keyviand <keyviandd2003@gmail.com>
2026-06-16 21:06:05 -0400

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NewHireCompanyManager
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ NewCompany.aspx.designer.cs
@@ -0,0 +1,65 @@
+namespace NewHireCompanyManager
+{
+    public partial class NewCompany
+    {
+        protected global::System.Web.UI.WebControls.Literal PageTitleLiteral;
+
+        protected global::System.Web.UI.WebControls.ValidationSummary ValidationSummary1;
+
+        protected global::System.Web.UI.WebControls.Label MessageLabel;
+
+        protected global::System.Web.UI.WebControls.TextBox CompanyNameTextBox;
+
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator CompanyNameValidator;
+
+        protected global::System.Web.UI.WebControls.TextBox CompanyIdentifierTextBox;
+
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator CompanyIdentifierValidator;
+
+        protected global::System.Web.UI.WebControls.FileUpload LogoUpload;
+
+        protected global::System.Web.UI.WebControls.Literal ExistingLogoLiteral;
+
+        protected global::System.Web.UI.WebControls.TextBox PhoneNumberTextBox;
+
+        protected global::System.Web.UI.WebControls.TextBox Address1TextBox;
+
+        protected global::System.Web.UI.WebControls.TextBox Address2TextBox;
+
+        protected global::System.Web.UI.WebControls.TextBox CityTextBox;
+
+        protected global::System.Web.UI.WebControls.DropDownList StateDropDown;
+
+        protected global::System.Web.UI.WebControls.TextBox ZipTextBox;
+
+        protected global::System.Web.UI.WebControls.TextBox PrimaryContactTextBox;
+
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator PrimaryContactValidator;
+
+        protected global::System.Web.UI.WebControls.TextBox EmailTextBox;
+
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator EmailRequiredValidator;
+
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator EmailFormatValidator;
+
+        protected global::System.Web.UI.WebControls.TextBox TaxIdTextBox;
+
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator TaxIdValidator;
+
+        protected global::System.Web.UI.WebControls.TextBox BankNameTextBox;
+
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator BankNameValidator;
+
+        protected global::System.Web.UI.WebControls.CheckBox Form1099CheckBox;
+
+        protected global::System.Web.UI.WebControls.TextBox WithholdingNumberTextBox;
+
+        protected global::System.Web.UI.WebControls.TextBox CountyTextBox;
+
+        protected global::System.Web.UI.WebControls.CheckBoxList ModulesCheckBoxList;
+
+        protected global::System.Web.UI.WebControls.Button SaveButton;
+
+        protected global::System.Web.UI.WebControls.Button CancelButton;
+    }
+}
